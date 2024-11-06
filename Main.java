c005f0e54373252079bf4e5212938229358693c8
Naelah Alshibani <146096692+naellash00@users.noreply.github.com>
2024-11-06 14:05:15 +0300

    Question 1 Main Class

--- Main.java
+++ Main.java
@@ -2,36 +2,30 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        MovablePoint mvp1 = new MovablePoint(10, 20, 15, 25);
-        System.out.println("X: " + mvp1.getX() + "\nX Speed: " + mvp1.getxSpeed());
-        System.out.println("Y: " + mvp1.getY() + "\nY Speed: " + mvp1.getySpeed());
-        mvp1.moveLeft();
-        System.out.println("Move Left: " + mvp1.getX());
-        mvp1.moveRight();
-        System.out.println("Move right: " + mvp1.getX());
-        mvp1.moveUp();
-        System.out.println("Move Up: " + mvp1.getY());
-        mvp1.moveDown();
-        System.out.println("Move Down: " + mvp1.getY());
-        System.out.println("************************");
-        MovablePoint mvp2 = new MovablePoint(5, 5, 5, 5);
-        mvp2.moveRight(); //10
-        System.out.println("Move Right: " + mvp2.getX());
-        mvp2.moveLeft(); //5
-        System.out.println("Move Left: " + mvp2.getX());
-        mvp2.moveUp(); // 10;
-        System.out.println("Move Up: " + mvp2.getY());
-        mvp2.moveDown(); // 5
-        System.out.println("Move Down: " + mvp2.getY());
-        mvp2.setxSpeed(10);
-        mvp2.setySpeed(10);
-        System.out.println("afet changing speed: ");
-        System.out.println("Move Right: " + mvp2.getX());
-        mvp2.moveLeft(); //5
-        System.out.println("Move Left: " + mvp2.getX());
-        mvp2.moveUp(); // 10;
-        System.out.println("Move Up: " + mvp2.getY());
-        mvp2.moveDown(); // 5
-        System.out.println("Move Down: " + mvp2.getY());
+        //(1). Test Book:
+        Book book1 = new Book("power", 75.8, "tom");
+        System.out.println("Book price before discount: " + book1.getPrice());
+        book1.getDiscount();
+        System.out.println("Book price after discount: " + book1.getPrice());
+        Book book2 = new Book();
+        book2.setName("woods");
+        book2.setPrice(50.6);
+        book2.setAuthor("sylvia");
+        System.out.println("Book2 name: " + book2.getName() + "\nAnd author: " + book2.getAuthor());
+        System.out.println("Book2 Before discount: " + book2.getPrice());
+        book2.getDiscount();
+        System.out.println("Book2 after discount: " + book2.getPrice());
+
+        System.out.println("*****************************************");
+        // (2). Test Movie:
+        Movie movie1 = new Movie();
+        movie1.setPrice(120);
+        System.out.println("Movie1 price before discount: " + movie1.getPrice());
+        movie1.getDiscount();
+        System.out.println("Movie1 price after discount: " + movie1.getPrice());
+        Movie movie2 = new Movie("dreams", 95, "lily");
+        System.out.println("Movie2 name: " + movie2.getName() + "\nAnd director: " + movie2.getDirector());
+
+
     }
 }
